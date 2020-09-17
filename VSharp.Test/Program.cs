8f2df7fa48d008f63517abe26e3cdda9dff5e0bc
Robin De Zwaef <robin.de.zwaef@telenet.be>
2020-09-18 01:00:28 +0200

    Fix project structure

---
+++ VSharp.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using VSharp.Models;
+
+namespace VSharp.Test
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string json = await client.GetStringAsync("https://api-vfan.vlive.tv/v3/board.21/posts?&app_id=x&limit=20&_=1600374693642");
+            VLivePostResponse postResponse = JsonConvert.DeserializeObject<VLivePostResponse>(json);
+
+            json = JsonConvert.SerializeObject(postResponse, Formatting.Indented);
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+}
